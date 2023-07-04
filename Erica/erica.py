f547f52df2cb76abe8203d7963001f796ba3e35a
samrat-p <mitra.stark.68@gmail.com>
2023-07-05 03:20:42 +0530

    Node erica

--- Erica/erica.py
+++ Erica/erica.py
@@ -1,12 +1,24 @@
 import discord
+from discord.ext import commands
 import requests
 
-erica = discord.Client()
+intents = discord.Intents.default()
+intents.messages = True
+erica = commands.Bot(command_prefix='!', intents=intents)
 
-async def login():
-    print(f'Logged in as {bot.user.name}')
+@erica.event
+async def on_ready():
+    print(f'Logged in as {erica.user.name}')
+
+@erica.command()
 async def recent_commits(ctx, repo_owner, repo_name):
     url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/commits'
-    headers = {'authorization':'bearer UR_GITHUB_TOKEN'}
-    responses = requests.get(url, headers=headers)
-    commits =responses.json()
+    headers = {'Authorization': 'Bearer YOUR_GITHUB_TOKEN'}
+    response = requests.get(url, headers=headers)
+    commits = response.json()
+
+    for commit in commits:
+        message = f'{commit["commit"]["message"]} - {commit["commit"]["committer"]["name"]}'
+        await ctx.send(message)
+
+erica.run('MTEyNTg3NTE3MjE4MDc3MDkzOA.GtC2yF.LtaGDjbxPS-aCRqkBWDv_BTTpoJkTA4e5kEBoA')
